1631f8910bcbe3165d86fff68cde65117b110b7e
Richard Haines <richard.haines@88.131.110>
2019-12-03 21:36:29 +0100

    adding packages

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,31 @@
 module.exports = {
     siteMetadata: {
-      title: `justask.dev`
+      title: 'justask.dev'
     },
-    plugins: [`gatsby-plugin-mdx`]
+    plugins: [
+        'gatsby-plugin-mdx',
+        {
+            resolve: 'gatsby-plugin-google-fonts',
+            options: {
+              fonts: [
+                'Josefin Sans',
+                'Poppins',
+                'Open Sans',
+                'source sans pro\:300,400,400i,700' 
+              ]
+            }
+        },
+        {
+            resolve: 'gatsby-theme-seo', 
+            options: {
+                title: 'justask.dev',
+                description: 'A website that provides answers to simple dev questions.',
+                author: 'Rich Haines',
+                siteUrl: 'https://justask.dev',
+            }
+        },
+        'gatsby-plugin-emotion',
+        'gatsby-plugin-sitemap',
+        'gatsby-plugin-robots-txt'
+    ]
   }
